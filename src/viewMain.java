c8384ee10298b87714099fe6eac3383ae1b22885
GromDv <46959198+GromDv@users.noreply.github.com>
2023-03-26 20:15:10 +0300

    Delete viewMain.java

--- src/viewMain.java
+++
@@ -1,9 +0,0 @@
-import controller.controller;
-
-public class viewMain {
-
-    public static void main(String[] args) {
-        controller myApp = new controller();
-        myApp.Start();
-    }
-}
